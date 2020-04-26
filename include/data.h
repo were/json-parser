c4075888c4ec333aa318156d563569e20d237b65
Jian Weng <jian.weng@ucla.edu>
2020-04-26 16:52:15 -0700

    it is compiling but crashing

--- include/data.h
+++ include/data.h
@@ -1,11 +1,15 @@
+<<<<<<< HEAD
 #pragma once
 
+=======
+>>>>>>> it is compiling but crashing
 #include <vector>
 #include <string>
 #include <map>
 
 namespace json {
 
+<<<<<<< HEAD
 class BaseNode;
 
 }
@@ -35,12 +39,35 @@ class Value : public BaseNode {
   ~Value() {}
   friend class BaseNode;
   inline void Accept(BaseVisitor *visitor) override;
+=======
+class BaseNode {
+  virtual ~BaseNode() {}
+  template<typename T> T* As();
+};
+
+template<typename T>
+class Value : BaseNode {
+ public:
+  using DataType = T;
+>>>>>>> it is compiling but crashing
  private:
   DataType data;
 };
 
+<<<<<<< HEAD
 template<typename T>
 inline T* BaseNode::As() {
+=======
+using KVPair = Value<std::map<std::string, BaseNode*>>;
+using Int = Value<int64_t>;
+using Float = Value<double>;
+using Bool = Value<bool>;
+using String = Value<std::string>;
+using Array = Value<std::vector<BaseNode*>>;
+
+template<typename T>
+T* BaseNode::As() {
+>>>>>>> it is compiling but crashing
   if (auto cast = dynamic_cast<Value<T>*>(this)) {
     return &cast->data;
   }
