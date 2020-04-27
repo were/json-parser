22c01d8a0aad80e572be34ad4b295fdd5161e6b0
Jian Weng <jian.weng@ucla.edu>
2020-04-27 15:08:51 -0700

    Add prefix to APIs to avoid multi-parser conflict, and enable -fPIC (#8)
    * a new prefix
    
    * add a prefix to the parser
    
    * fpic

--- test/main.cpp
+++ test/main.cpp
@@ -1,12 +1,13 @@
 #include <cstdio>
+#include "json.lex.h"
 #include "json.tab.h"
 
 void yyrestart(FILE*);
 
 int main(int argc, char *argv[]) {
    params p;
-   yyrestart(fopen(argv[1], "r"));
-   yyparse(&p);
+   JSONrestart(fopen(argv[1], "r"));
+   JSONparse(&p);
    json::JSONPrinter printer(std::cout);
    p.data->Accept(&printer);
    delete p.data;
