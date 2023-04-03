9caa1bf60514bf9e7722652dfc6364cd72725443
dkupsh <dkupsh@ucla.edu>
2023-04-02 19:08:16 -0700

    fixed test

--- tests/create.cpp
+++ tests/create.cpp
@@ -7,18 +7,17 @@
 void yyrestart(FILE*);
 
 int main(int argc, char* argv[]) {
-  Json::Value data;
+  Json::Value data(new Json::ValueObject({}));
 
   data["string"] = "Hello There";
+
+  std::cout << data << std::endl;
 
   if (!data.isMember("string")) {
     std::cerr << "string member not found" << std::endl;
     return 1;
-  } else if (!data["string"].isString()) {
-    std::cerr << "string member is not string" << std::endl;
-    return 1;
   } else if (data["string"].asString() != "Hello There") {
-    std::cerr << "string member is not 'Hello There'" << std::endl;
+    std::cerr << "string member has wrong value" << std::endl;
     return 1;
   }
 
